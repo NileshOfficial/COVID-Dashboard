2bb0132aead7155e407def3950bf3edb05e4e378
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-09 14:40:54 +0530

    initiated home component and case count component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CaseCountComponent } from './home/case-count/case-count.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CaseCountComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/case-count/case-count.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'covid-case-count',
+  templateUrl: './case-count.component.html',
+  styleUrls: ['./case-count.component.css']
+})
+export class CaseCountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
