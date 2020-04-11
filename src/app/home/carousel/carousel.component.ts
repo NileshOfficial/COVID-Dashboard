2a6beaa6c2bbb6d3ef80cae2dac9da9038a7fafe
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-11 22:19:22 +0530

    carousel component layout complete

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'covid-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements OnInit, OnDestroy {
+
+  currentCarouselBanner = 0;
+  private intervalHandle: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.intervalHandle = setInterval(this.rotateBanners.bind(this), 2000);
   }
 
+  rotateBanners() {
+    this.currentCarouselBanner = (this.currentCarouselBanner + 1) % 3;
+  }
+
+  changeBanner(idx: number) {
+    this.currentCarouselBanner = idx;
+    clearInterval(this.intervalHandle);
+    this.intervalHandle = setInterval(this.rotateBanners.bind(this), 2000);
+  }
+
+  startRotation() {
+    this.intervalHandle = setInterval(this.rotateBanners.bind(this), 2000);
+  }
+
+  stopRotation() {
+    clearInterval(this.intervalHandle);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalHandle);
+  }
 }
