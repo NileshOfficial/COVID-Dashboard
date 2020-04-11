0e1f36231834782343b52b99e3c55210a1fb2241
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-11 15:05:36 +0530

    directive complete

--- src/app/directives/active-emphasis.directive.ts
+++ src/app/directives/active-emphasis.directive.ts
@@ -1,13 +1,20 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
-import { AfterViewInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[activeEmphasis]'
 })
-export class ActiveEmphasisDirective implements AfterViewInit {
+export class ActiveEmphasisDirective {
+  @Input() activeEmphasis: string;
+
+  @HostListener('click') setAsActive() {
+    let parent: HTMLElement = this.renderer.parentNode(this.element.nativeElement);
+    let child: HTMLElement = parent.querySelector('.' + this.activeEmphasis);
+    
+    if (child)
+      this.renderer.removeClass(child, this.activeEmphasis);
+    
+      this.renderer.addClass(this.element.nativeElement, this.activeEmphasis);
+  }
 
   constructor(private element: ElementRef, private renderer: Renderer2) { }
-
-  ngAfterViewInit() {
-  }
 }
