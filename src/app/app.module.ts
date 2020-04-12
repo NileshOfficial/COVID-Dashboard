430020501e0f37650f89cbe6180b98196845d23e
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-12 13:55:58 +0530

    initiated component accordian

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccordianComponent } from './accordian/accordian.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordianComponent
   ],
   imports: [
     BrowserModule,
