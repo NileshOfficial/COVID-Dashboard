59990dc2408f00e6e71aa6a5db7dca66357dd540
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-12 15:59:19 +0530

    initiated component help-line

---
+++ src/app/help-line/help-line.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'covid-help-line',
+  templateUrl: './help-line.component.html',
+  styleUrls: ['./help-line.component.css']
+})
+export class HelpLineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
