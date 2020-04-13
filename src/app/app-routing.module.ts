df6aceb50e332d93b71ddf6899c20cfef73c5620
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-13 15:25:54 +0530

    hooked not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AccordianComponent } from './accordian/accordian.component';
 import { HelpLineComponent } from './help-line/help-line.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 const routes: Routes = [
   {path: "", redirectTo: '/Dashboard', pathMatch: "full"},
   {path: "Dashboard", component: HomeComponent},
   {path: "FrequentQuestions", component: AccordianComponent},
-  {path: "HelpLinks", component: HelpLineComponent}
+  {path: "HelpLinks", component: HelpLineComponent},
+  {path: "BadRequest", component: NotfoundComponent},
+  {path: "**", redirectTo: "/BadRequest"}
 ];
 
 @NgModule({
