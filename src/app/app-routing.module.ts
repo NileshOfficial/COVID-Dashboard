b83c06414215ce1c8363b13bd48553cdf8918203
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-13 13:44:42 +0530

    hooked routes with navlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AccordianComponent } from './accordian/accordian.component';
+import { HelpLineComponent } from './help-line/help-line.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: '/Dashboard', pathMatch: "full"},
+  {path: "Dashboard", component: HomeComponent},
+  {path: "FrequentQuestions", component: AccordianComponent},
+  {path: "HelpLinks", component: HelpLineComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
