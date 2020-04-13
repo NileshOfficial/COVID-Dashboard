27f47ca3806cf7b301aecc1a67c00e537db7c04b
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-14 00:05:28 +0530

    added api urls

--- src/app/services/httpUtils/http.config.ts
+++ src/app/services/httpUtils/http.config.ts
@@ -1,4 +1,5 @@
 export const url = {
-    globalCasesCountUrl: "",
-    countryWiseCasesCountUrl: ""
+    globalCasesCountUrl: "https://corona.lmao.ninja/all",
+    countryWiseCasesCountUrl: "https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search",
+    mapDataUrl: "https://www.trackcorona.live/api/countries/"
 }
