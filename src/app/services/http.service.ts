fad6aef4a4cfc9bcfaa8f7027e8ccbc9d05d8603
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-14 11:58:16 +0530

    restructured http service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { url } from './http.config';
+import { globalCasesData } from './response.model';
 
-@Injectable({providedIn: 'root'})
-export class HttpService {
-    constructor(private httpClient: HttpClient) { }
-    
+@Injectable({
+    providedIn: 'root'
+})
+
+export class HttpUtil {
+    constructor(private http: HttpClient) { }
+
+    getGlobalCasesCount() {
+        return this.http.get<globalCasesData>(url.globalCasesCountUrl);
+    }
+
+    getCountryWiseCasesCount() {
+        return this.http.get(url.countryWiseCasesCountUrl);
+    }
+
+    getMapData() {
+        return this.http.get(url.mapDataUrl);
+    }
 }
