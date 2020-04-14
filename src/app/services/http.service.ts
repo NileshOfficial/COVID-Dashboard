1af13aa7b2b1f1076c8fc3835caa577442beb69f
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-14 20:35:18 +0530

    mapped response data of getGlobalCasesCount

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { url } from './http.config';
 import { globalCasesData } from './response.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -10,7 +11,14 @@ export class HttpService {
     constructor(private http: HttpClient) { }
 
     getGlobalCasesCount() {
-        return this.http.get<globalCasesData>(url.globalCasesCountUrl);
+        return this.http.get<globalCasesData>(url.globalCasesCountUrl).pipe(map(res => {
+            return {
+                cases: res.cases,
+                recovered: res.recovered,
+                active: res.active,
+                deaths: res.deaths
+            }
+        }));
     }
 
     getCountryWiseCasesCount() {
