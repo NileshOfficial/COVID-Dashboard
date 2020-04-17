4d59dd1ecc1d95d5219c10481abcfab7bdd99550
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-17 21:26:01 +0530

    embedded twitter timeline

--- src/app/home/tweets/tweets.component.ts
+++ src/app/home/tweets/tweets.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'covid-tweets',
   templateUrl: './tweets.component.html',
   styleUrls: ['./tweets.component.css']
 })
-export class TweetsComponent implements OnInit {
+export class TweetsComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    // @ts-ignore
+    twttr.widgets.load();
+  }
 }
