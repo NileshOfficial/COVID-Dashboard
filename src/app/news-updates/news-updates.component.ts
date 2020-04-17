813752373fafdcdfdee836ba8e68d4a25e72ef5b
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-17 11:55:36 +0530

    hooked loader and error message in news component

--- src/app/news-updates/news-updates.component.ts
+++ src/app/news-updates/news-updates.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import { newsData }  from '../services/response.model';
-import { faCircle, faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
+import { faCircle, faGlobeAmericas, IconDefinition, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'covid-news-updates',
@@ -11,15 +11,29 @@ import { faCircle, faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
 export class NewsUpdatesComponent implements OnInit {
 
   newsData: newsData = null;
-  separatorIcon=faCircle;
-  globeIcon=faGlobeAmericas;
+  separatorIcon: IconDefinition = faCircle;
+  globeIcon: IconDefinition = faGlobeAmericas;
+  arrow: IconDefinition = faArrowRight;
+  errMessage = '';
 
   constructor(private http: HttpService) { }
 
   ngOnInit(): void {
-    this.http.getNews().subscribe(data => {
-      this.newsData = data;
-    })
+    this.fetchArticles();
+  }
+
+  fetchArticles() {
+    this.http.getNews().subscribe(this.updateArticles.bind(this), this.errHandler.bind(this));
+  }
+
+  updateArticles(data) {
+    this.newsData = data;
+  }
+
+  errHandler(err: ErrorEvent) {
+    console.log(err);
+    if(!this.newsData)
+      this.errMessage = "error occurred, try refreshing the page."
   }
 
   getArticles() {
