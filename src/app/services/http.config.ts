c42b89ed98e44aa139cbc8dac588c0d0e46c0bdf
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-17 12:41:37 +0530

    completed carousel linking with news and update

--- src/app/services/http.config.ts
+++ src/app/services/http.config.ts
@@ -1,5 +1,5 @@
 export const url = {
-    globalCasesCountUrl: "https://corona.lmao.ninja/all",
+    globalCasesCountUrl: "https://corona.lmao.ninja/v2/all",
     countryWiseCasesCountUrl: "https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search",
     mapDataUrl: "https://www.trackcorona.live/api/countries/",
     newsUrl: "https://newsapi.org/v2/top-headlines?q=corona+virus&sources=google-news,bbc-news,business-insider"
